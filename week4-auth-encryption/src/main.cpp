d24c4663d7601679b2893d0f1879bc1efcd1c046
Krystian Heberlein <krystianheberlein@gmail.com>
2019-02-09 23:38:34 +0100

    week4 added http client

--- week4-auth-encryption/src/main.cpp
+++ week4-auth-encryption/src/main.cpp
@@ -2,7 +2,7 @@
 #include <curl/curl.h>
 #include <string>
 #include <iostream>
-#include "message.hpp"
+#include "paddingoracle.hpp"
 
 constexpr auto url =
   "http://crypto-class.appspot.com/po?er=";
@@ -17,7 +17,7 @@ int main(void)
 {
   Logger::getInstance().init();
 
-  Message m("f20bdba6ff29eed7b046d1df9fb70000"
+  PaddingOracle m("f20bdba6ff29eed7b046d1df9fb70000"
             "58b1ffb4210a580f748b4ac714c001bd"
             "4a61044426fb515dad3f21f18aa577c0"
             "bdf302936266926ff37dbf7035d5eeb2");
